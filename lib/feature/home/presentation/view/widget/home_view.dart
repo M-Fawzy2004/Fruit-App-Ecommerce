22cd462f5b0b8c1ce1ec5e748888c5b29bbbcb9e
Mohamed Fawzy <mofawzy.com7@gmail.com>
2025-02-13 11:49:33 +0200

    add bloc provider with home view

--- lib/feature/home/presentation/view/widget/home_view.dart
+++ lib/feature/home/presentation/view/widget/home_view.dart
@@ -1,11 +1,20 @@
+import 'package:e_commerce_app/core/cubit/get_product/get_product_cubit.dart';
+import 'package:e_commerce_app/core/repos/product_repo.dart';
+import 'package:e_commerce_app/core/services/get_it_service.dart';
 import 'package:e_commerce_app/feature/home/presentation/view/widget/home_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return HomeViewBody();
+    return BlocProvider(
+      create: (context) => GetProductCubit(
+        getIt.get<ProductRepo>(),
+      ),
+      child: HomeViewBody(),
+    );
   }
 }
