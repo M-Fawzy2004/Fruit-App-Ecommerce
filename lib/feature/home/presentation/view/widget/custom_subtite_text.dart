8a0d42054e1062d455ae65498db62a5ffbce5fd3
Mohamed Fawzy <mofawzy.com7@gmail.com>
2025-02-14 09:17:46 +0200

    display fruit item

---
+++ lib/feature/home/presentation/view/widget/custom_subtite_text.dart
@@ -0,0 +1,43 @@
+
+import 'package:e_commerce_app/core/entities/product_entity.dart';
+import 'package:e_commerce_app/core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomSubtitleText extends StatelessWidget {
+  const CustomSubtitleText({
+    super.key,
+    required this.productEntity,
+  });
+
+  final ProductEntity productEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child: Text.rich(
+        TextSpan(
+          children: [
+            TextSpan(
+              text: productEntity.price.toString(),
+              style: Styles.fontText16(context).copyWith(
+                color: Color(0xffF4A91F),
+              ),
+            ),
+            TextSpan(
+              text: " / ",
+              style: Styles.fontText16(context).copyWith(
+                color: Color(0xffF4A91F),
+              ),
+            ),
+            TextSpan(
+              text: 'لكل كيلو',
+              style: Styles.fontText13(context).copyWith(
+                color: Color(0xffF8C76D),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/feature/home/presentation/view/widget/fruit_item.dart
+++ lib/feature/home/presentation/view/widget/fruit_item.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:e_commerce_app/core/entities/product_entity.dart';
 import 'package:e_commerce_app/core/utils/app_styles.dart';
 import 'package:e_commerce_app/core/utils/assets.dart';
+import 'package:e_commerce_app/feature/home/presentation/view/widget/custom_subtite_text.dart';
 import 'package:flutter/material.dart';
 
 class FruitItem extends StatelessWidget {
@@ -29,13 +31,26 @@ class FruitItem extends StatelessWidget {
             child: Column(
               children: [
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.04,
+                  height: MediaQuery.of(context).size.height * 0.05,
                 ),
-                Center(
-                  child: Image.asset(
-                    Assets.imagesPineappleCuate,
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: CachedNetworkImage(
                     height: 100,
+                    width: MediaQuery.sizeOf(context).width * .4,
+                    imageUrl: productEntity.imageUrl ?? '',
                     fit: BoxFit.cover,
+                    placeholder: (context, url) => const Icon(
+                      Icons.image,
+                      size: 25,
+                      color: Colors.grey,
+                    ),
+                    errorWidget: (context, url, error) => Image.asset(
+                      '',
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
                 ListTile(
@@ -43,32 +58,7 @@ class FruitItem extends StatelessWidget {
                     productEntity.name,
                     style: Styles.fontText13(context),
                   ),
-                  subtitle: FittedBox(
-                    child: Text.rich(
-                      TextSpan(
-                        children: [
-                          TextSpan(
-                            text: '20 جنيه',
-                            style: Styles.fontText16(context).copyWith(
-                              color: Color(0xffF4A91F),
-                            ),
-                          ),
-                          TextSpan(
-                            text: " / ",
-                            style: Styles.fontText16(context).copyWith(
-                              color: Color(0xffF4A91F),
-                            ),
-                          ),
-                          TextSpan(
-                            text: 'لكل كيلو',
-                            style: Styles.fontText13(context).copyWith(
-                              color: Color(0xffF8C76D),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                  subtitle: CustomSubtitleText(productEntity: productEntity),
                   trailing: CircleAvatar(
                     backgroundColor: Color(0xff1B5E37),
                     child: IconButton(
